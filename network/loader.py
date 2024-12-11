d722a4a8d9c271a1e00593a53192abae4c38ba9b
ngiatsog <ngiatsog@iti.gr>
2024-12-11 16:32:05 +0200

    load_graph without knowing about nodes, simplified caching

--- network/loader.py
+++ network/loader.py
@@ -6,25 +6,22 @@ from pathlib import Path
 from utils import analytic_ppr
 
 
-METADATA = {
-    "gnutella": {
-        "url": "http://snap.stanford.edu/data/p2p-Gnutella08.txt.gz",
-        "delimiter": "\t",
-    },
-    "fb": {
-        "url": "https://snap.stanford.edu/data/facebook_combined.txt.gz",
-        "delimiter": " ",
-    },
-    "internet": {
-        "url": "https://snap.stanford.edu/data/as20000102.txt.gz",
-        "delimiter": "\t",
-    },
+URLS = {
+    "gnutella": "http://snap.stanford.edu/data/p2p-Gnutella08.txt.gz",
+    "fb": "https://snap.stanford.edu/data/facebook_combined.txt.gz",
+    "internet": "https://snap.stanford.edu/data/as20000102.txt.gz",
+}
+
+TOY_GRAPH_PARAMETERS = {
     "toy_erdos": {"n": 50, "p": 0.2},
     "toy_watts_strogatz": {"n": 50, "k": 3, "p": 0.2},
 }
-COMMON_DELIMITER = ";"
 
-def load_graph(dataset="fb", node_init=None):
+def load_network(dataset, node_init, ppr_a):
+    pass
+
+
+def load_graph(dataset="fb"):
     """
     Loads a dataset graph as an networkx.Graph object.
     The first time the dataset is downloaded and
@@ -37,28 +34,15 @@ def load_graph(dataset="fb", node_init=None):
     Returns:
         networkx.Graph: An object representing the dataset graph.
     """
-    if node_init is None:
-        node_init = lambda x:x
 
-    graph = nx.Graph()
-    node_dict = dict()
-
-    dset_path =  Path(__file__).parent / ".cache" / dataset
+    dset_path = Path(__file__).parent / ".cache" / dataset
     dset_path.mkdir(parents=True, exist_ok=True)
     edgelist_path = dset_path / "edgelist.csv"
     if not edgelist_path.exists():
-        print("[graph loader]: graph is not cached, will download")
-        download(dataset, edgelist_path)
+        print(f'generating and caching graph "{dataset}"')
+        generate(dataset, edgelist_path)
+    return nx.read_edgelist(edgelist_path)
 
-    with open(edgelist_path) as file:
-        for line in file:
-            node_names = line[:-1].split(";")
-            for node_name in node_names:
-                if node_name not in node_dict:
-                    node_dict[node_name] = node_init(node_name)
-            graph.add_edge(node_dict[node_names[0]], node_dict[node_names[1]])
-            graph.add_edge(node_dict[node_names[1]], node_dict[node_names[0]])
-    return graph
 
 
 def load_ppr_matrix(dataset, alpha, symmetric=True, _adjacency_matrix=None):
@@ -76,23 +60,29 @@ def load_ppr_matrix(dataset, alpha, symmetric=True, _adjacency_matrix=None):
     Returns:
         networkx.Graph: An object representing the dataset graph.
     """
-    dset_path =  Path(__file__).parent / ".cache" / dataset
+    dset_path = Path(__file__).parent / ".cache" / dataset
     dset_path.mkdir(exist_ok=True)
-    pprmat_path = dset_path / f"pprmat_alpha{alpha}_{'symm' if symmetric else 'asymm'}.npy"
+    pprmat_path = (
+        dset_path / f"pprmat_alpha{alpha}_{'symm' if symmetric else 'asymm'}.npy"
+    )
 
     if pprmat_path.exists():
         return np.load(pprmat_path)
 
     print("[ppr matrix loader]: diffusion matrix is not cached, will compute")
-    
-    adj = nx.adjacency_matrix(load_graph(dataset)) if _adjacency_matrix is None else _adjacency_matrix
+
+    adj = (
+        nx.adjacency_matrix(load_graph(dataset))
+        if _adjacency_matrix is None
+        else _adjacency_matrix
+    )
     ppr_matrix = analytic_ppr(adj, alpha, symmetric)
 
     np.save(pprmat_path, ppr_matrix)
     return ppr_matrix
 
 
-def download(dataset, edgelist_path):
+def generate(dataset, edgelist_path):
     """
     Downloads the edgelist of a graph dataset to filepath.
 
@@ -100,30 +90,20 @@ def download(dataset, edgelist_path):
         dataset (str): The name of the dataset. Available names are found in METADATA.
         filepath (str): The path where to download the dataset edgelist. The standard path is given by get_edgelist_path().
     """
-    
-    if dataset in ["gnutella", "fb", "internet"]:
-        url = METADATA[dataset]["url"]
-        print(f"[graph generation script]: downloading {dataset} graph from {url}")
+
+    if dataset in URLS:
+        url = URLS[dataset]
+        print(f'[graph generation script]: downloading graph "{dataset}" from {url}')
         res = requests.get(url, allow_redirects=True)
         print(f"[graph generation script]: decompressing graph")
         data = gzip.decompress(res.content)
-        print(f"[graph generation script]: saving graph in common format")
+        print(f"[graph generation script]: caching graph as edgelist")
         with open(edgelist_path, "wb") as f:
             f.write(data)
-
-        f = open(edgelist_path, "r", encoding="utf8")
-        lines = f.readlines()
-        f.close()
-
-        with open(edgelist_path, "w") as f:
-            for line in lines:
-                if line.startswith("#"):
-                    continue
-                f.write(line.replace(METADATA[dataset]["delimiter"], COMMON_DELIMITER))
         print(f"[graph generation script]: finished!")
 
     elif dataset == "toy_erdos":
-        n, p = METADATA[dataset]["n"], METADATA[dataset]["p"]
+        n, p = TOY_GRAPH_PARAMETERS[dataset]["n"], TOY_GRAPH_PARAMETERS[dataset]["p"]
         g = nx.gnp_random_graph(n, p)
         while not nx.is_connected(g):
             p = min(1, 1.01 * p)
@@ -131,17 +111,17 @@ def download(dataset, edgelist_path):
 
         with open(edgelist_path, "w") as f:
             for e in g.edges:
-                f.write(f"{e[0]}{COMMON_DELIMITER}{e[1]}\n")
+                f.write(f"{e[0]} {e[1]}\n")
 
     elif dataset == "toy_watts_strogatz":
-        n, k, p = METADATA[dataset]["n"], METADATA[dataset]["k"], METADATA[dataset]["p"]
+        n, k, p = TOY_GRAPH_PARAMETERS[dataset]["n"], TOY_GRAPH_PARAMETERS[dataset]["k"], TOY_GRAPH_PARAMETERS[dataset]["p"]
         g = nx.connected_watts_strogatz_graph(n, k, p)
 
         with open(edgelist_path, "w") as f:
             for e in g.edges:
-                f.write(f"{e[0]}{COMMON_DELIMITER}{e[1]}\n")
+                f.write(f"{e[0]} {e[1]}\n")
 
     else:
         raise Exception(
-            f"unknown dataset '{dataset}', known datasets: {list(METADATA)}"
+            f"unknown graph \"{dataset}\", try from {list(URLS)} or {list(TOY_GRAPH_PARAMETERS)}"
         )
