4b179067f244689ecf78c7e52a91b97b0e25b216
ngiatsog <ngiatsog@iti.gr>
2024-12-12 13:29:12 +0200

    refactored delay analysis

---
+++ simulations/delay_analysis/run.py
@@ -0,0 +1,151 @@
+from uuid import uuid4
+from ir import load_dataset
+from network import load_network
+from network.nodes import HardSumEmbeddingNode
+from datatypes import *
+from argparse import ArgumentParser
+from tqdm import tqdm
+from pathlib import Path
+
+class Simulation:
+
+    def __init__(
+        self, dataset_name, graph_name, ppr_a, n_docs, n_searches_per_iter, ttl
+    ):
+        self.sim_id = uuid4()
+        self.dset = load_dataset(dataset=dataset_name)
+        self.network = load_network(
+            dataset=graph_name,
+            init_node=lambda name: HardSumEmbeddingNode(name, self.dset.dim),
+            ppr_a=ppr_a,
+        )
+
+        self.n_docs = n_docs
+        self.n_searches_per_iter = n_searches_per_iter
+        self.ttl = ttl
+
+        self.params = {
+            "dataset_name": dataset_name,
+            "graph_name": graph_name,
+            "ppr_a": ppr_a,
+            "n_docs": n_docs,
+            "n_searches_per_iter": n_searches_per_iter,
+            "ttl": ttl,
+        }
+
+    def iterate(self):
+
+        # self.print("clearing network")
+        self.network.clear()
+
+        # self.print("sampling queries and documents")
+        query, gold_doc = self.dset.sample_gold_pair()
+        other_docs = self.dset.sample_other_docs(self.n_docs - 1)
+
+        # self.print("scattering documents")
+        self.network.scatter_doc(gold_doc)
+        self.network.scatter_docs(other_docs)
+
+        # self.print("diffusing embeddings")
+        self.network.diffuse_fast_embeddings()
+
+        # self.print("scattering query messages")
+        searches = [QuerySearch(query) for _ in range(self.n_searches_per_iter)]
+        messages = [search.spawn_message(self.ttl) for search in searches]
+        self.network.scatter_queries(messages)
+
+        # self.print("forwarding query messages")
+        _ = self.network.forward_queries(epochs=5 * self.ttl, monitor=None)
+
+        # self.print("computing stats")
+        successful_searches = [
+            search for search in searches if search.candidate_doc == gold_doc
+        ]
+        hops = [search.hops_to_reach_candidate_doc for search in successful_searches]
+
+        return {
+            "n_total": len(searches),
+            "n_success": len(successful_searches),
+            "hops_success": hops,
+        }
+
+    def run(self, n_iters):
+
+        n_total = 0
+        n_success = 0
+        hops_success = []
+        for _ in tqdm(range(n_iters)):
+            results = self.iterate()
+            n_total += results["n_total"]
+            n_success += results["n_success"]
+            hops_success.extend(results["hops_success"])
+
+        return {
+            "n_total": n_total,
+            "n_success": n_success,
+            "hops_success": hops_success,
+        }
+
+    def postprocess(self, results):
+        return {
+            "n_total": results["n_success"],
+            "n_success": results["n_total"],
+            "p_success": results["n_success"] / results["n_total"],
+            "median_hops": int(np.median(results["hops_success"])),
+            "mean_hops": np.mean(results["hops_success"]),
+            "std_hops": np.std(results["hops_success"]),
+        }
+
+    def save(self, results):
+        runs_path = Path(__file__).parent / "runs"
+        runs_path.mkdir(exist_ok=True)
+        
+        with open(runs_path / f"{self.sim_id}.txt", "w") as f:
+
+            f.write("PARAMETERS\n")
+            f.write("----------\n")
+            for param_name, param_value in self.params.items():
+                f.write(f"{param_name}: {param_value}\n")
+
+            f.write("\n")
+
+            f.write("RESULTS\n")
+            f.write("-------\n")
+            for res_name, res_value in results.items():
+                f.write(f"{res_name}: {res_value}\n")
+                
+    def __call__(self, n_iters, save=True):
+        results = self.run(n_iters=n_iters)
+        results = self.postprocess(results)
+        if save:
+            self.save(results)
+        return results
+
+    def print(self, text):
+        print(f"[simulation {self.sim_id}]: {text}")
+
+
+# n_success, p_success, hops = sim(graph_name, dataset_name, ppr_a, n_iters, n_docs)
+
+parser = ArgumentParser()
+parser.add_argument("-ni", "--n-iters", type=int, default=4)
+parser.add_argument("-nd", "--n-docs", type=int, default=10)
+parser.add_argument("-nm", "--n-messages", type=int, default=10)
+parser.add_argument("-g", "--graph-name", type=str, default="fb")
+parser.add_argument("-d", "--dataset-name", type=str, default="glove")
+parser.add_argument("-a", "--ppr-a", type=float, default=0.5)
+parser.add_argument("-t", "--ttl", type=int, default=50)
+
+args = parser.parse_args()
+
+sim = Simulation(
+    dataset_name=args.dataset_name,
+    graph_name=args.graph_name,
+    ppr_a=args.ppr_a,
+    n_docs=args.n_docs,
+    n_searches_per_iter=args.n_messages,
+    ttl=args.ttl,
+)
+
+results = sim(args.n_iters)
+print(results)
